1e5f1ca4afeab02bc87f59a12108b69871e5aea4
dhruvsol <dhruvrajsinghsolanki161@gmail.com>
2023-03-13 11:19:17 +0530

    feat: added comments

--- src/components/Projects/ProjectsDetail/ProjectComponents/Discussions/index.tsx
+++ src/components/Projects/ProjectsDetail/ProjectComponents/Discussions/index.tsx
@@ -1,13 +1,28 @@
+import { Button, HStack, Textarea, VStack } from '@chakra-ui/react';
+
 const Discussions = () => {
   return (
     <>
-      {/* <TopicView
-        showTitle={false}
-        forumId={{
-          forumID: 'FoXEtXAD9bL93RH5uFYt5rLnw9hZnenjQxxSBtoqDTTZ',
-        }}
-        topicId={1}
-      /> */}
+      <VStack w={'full'} mt={6}>
+        <Textarea
+          h={32}
+          minH={32}
+          placeholder="Start wrting here....."
+          border={'1px solid white'}
+        />
+        <HStack w={'full'} align={'end'} justify={'end'}>
+          <Button
+            backgroundColor={'#D645A6 !important'}
+            color="white"
+            py={{ base: '1rem', md: '1.2rem' }}
+            px={{ base: '1rem', md: '1.6rem' }}
+            fontSize={{ base: 'sm', md: 'md' }}
+            fontWeight="700"
+          >
+            Comment
+          </Button>
+        </HStack>
+      </VStack>
     </>
   );
 };
